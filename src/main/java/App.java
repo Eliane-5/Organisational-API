327ba72e27f3a34d7ec7110e155e46e4974dfa8e
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-01 19:28:41 +0200

    adding a gome pathway in app and its file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static spark.Spark.*;
+
+
 public class App {
+    public static void main(String[] args){
+//      Making a home pathway
+        get("/",(request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine() );
+    }
 }
