79923f7e759ee62d0287162e39ea658dd4106176
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-01 19:38:04 +0200

    adding app logic routing to create new user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,5 +14,22 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine() );
+//        create a new user form
+        get("/user/new", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newUser-form.hbs");
+        }, new HandlebarsTemplateEngine());
+//      post the user entered
+        post("/user/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String fName = request.queryParams("fName");
+            String sName = request.queryParams("sName");
+            String position = request.queryParams("position");
+            String department = request.queryParams("department");
+            Users newUser = new Users(fName,sName,position,department);
+            model.put("user", newUser);
+            newUser.save();
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
