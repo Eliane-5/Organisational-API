8ccaba44d5da347b5b56069147ec3c8f5b06d04c
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-03 02:43:25 +0200

    adding heroku necessary info

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,8 @@
 import org.sql2o.*;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news_portal", "eliane", "19601963");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/news_portal", "eliane", "19601963");
+String connectionString = "jdbc:postgresql://ec2-54-197-241-96.compute-1.amazonaws.com:5432/ddljfgmh6f392k"; //!
+    Sql2o sql2o = new Sql2o(connectionString, "hpbjbqclooplnk", "34ac0ce37c74bae21de7f36addc6250cbd862989713af26ca820b82355e825fd");
+
 }
